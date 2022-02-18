efa6ae8d5b235c828cc18a862af927decef90fa9
ed1van <edivan411@gmail.com>
2022-02-18 09:46:09 -0300

    Atualização do Blog Pessoal - Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'}, //toda vez que for vazio ele vai para o entrar
+
+  {path: "entrar", component:EntrarComponent}, //rota entrar
+  {path: "cadastrar", component:CadastrarComponent}, //rota cadastrar
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
